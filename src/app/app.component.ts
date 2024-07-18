8996be5f041ace60b35e7bdb3c7a52a992e86b29
sanduni <sanduniakn@gmail.com>
2024-07-18 20:07:02 +0530

    add to-do items for each users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from "@angular/common";
 
-type TodoItem = {//create a type
-
+type TodoItem = {//create a type alias in type script
+  id: number,
+  title: string,
+  completed: boolean,
+  userId: number
 }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  todoItems:TodoItem = []
+  findUser(userId: number){
+    return this.users.find(user => user.userId === userId)
+  }
+
+  users = [
+    {userId: 1, name: "Kasun"},
+    {userId: 2, name: "Nisali"},
+    {userId: 3, name: "Senuli"},
+    {userId: 4, name: "Chamin Wishanka"},
+    {userId: 5, name: "Dasun Madushanka"},
+  ]
+  //todoItems:{}
+  todoItems:TodoItem[] = [];
+  loading = true;
   constructor() {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
-      .then(todoItems => this.todoItems = todoItems);
+      .then(todoItems => this.todoItems = todoItems)
+      .finally(() => setTimeout(() => {
+        this.loading = false;
+      }, 2000));
+  }
+
+  trackItemFn(index:number, item: TodoItem): number {
+    return item.id;
   }
 }
